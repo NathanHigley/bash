f1f4f693dff64ed62f36f765003dc25ff7399ca5
NathanHigley <nathan@jhigley.com>
2019-02-04 08:47:25 -0800

    delete your account.

---
+++ gitconfig.sh
@@ -0,0 +1,6 @@
+git config --list;
+git config --global user.username (user);
+git config --global user.name (user);
+git config --global user.user (user);
+git config --global user.email (email);
+git config --list;
